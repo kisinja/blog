e471c5d6a1a13cdde9e5ca90dd3cc9e6271d6f56
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-03-01 17:54:33 +0300

    Changes to be committed: 	new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/app.js 	new file:   src/blog.html 	new file:   src/images/edited.jpeg 	new file:   src/images/football.jpg 	new file:   src/images/ig_logo.png 	new file:   src/images/kisinja.jpg 	new file:   src/images/matatu.jpg 	new file:   src/images/music.jpg 	new file:   src/images/rununu.jpg 	new file:   src/images/syrian.jpg 	new file:   src/images/tech.jpg 	new file:   src/images/ufc.jpg 	new file:   src/index.html 	new file:   src/input.css 	new file:   src/output.css 	new file:   src/profile.html 	new file:   src/video/porsche.mp4 	new file:   tailwind.config.js

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const navBar = document.querySelector('nav');
+const menu = document.querySelector('.menu');
+const accBtn = document.querySelector('.account');
+const links = document.querySelectorAll('#menu-links');
+
+console.log(accBtn);
+console.log(menu);
+console.log(links);
+
+accBtn.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
+
+
+window.onscroll = () => {
+    if (window.scrollY > 78) {
+        navBar.style.backgroundColor = 'white';
+        navBar.style.border = 'none';
+    }
+    else {
+        navBar.style.background = "rgb(243 ,244 ,246)";
+        navBar.style.borderBottom = '2px solid #e0e0e0';
+    }
+}
